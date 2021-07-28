b5a1227009d8da7b155394f290d31187d3407fcb
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-07-28 09:09:28 +0800

    3.3 - 新增页面

--- learn_ku/goblog/main.go
+++ learn_ku/goblog/main.go
@@ -9,11 +9,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello，这里是 GoBlog。</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+"<p>如有疑惑，请联系我们。</p>")
+	}
 }
 
 func main() {
-	// 用以指定处理 HTTP 请求的函数
+	// 用以指定处理 HTTP 请求的函数，/ 意味着任意路径
 	http.HandleFunc("/", handlerFunc)
 	// 用以监听本地 3000 端口并提供服务
 	http.ListenAndServe(":3000", nil)
