3aa33d8a41a50b649ebb0611cffeea2c8e98d1c6
sebastiankennedy <sebastiankennedy@foxmail.com>
2021-07-28 09:03:16 +0800

    3.2 - 一个简单的 Go Web 程序

--- learn_ku/goblog/main.go
+++ learn_ku/goblog/main.go
@@ -1,5 +1,8 @@
+// 每一段 Go 程序都 必须 属于一个包。而 main 包在 Go 程序中有特殊的位置
+// 一个标准的可执行的 Go 程序必须有 package main 的声明。
 package main
 
+// import 关键词引入 Go Package
 import (
 	"fmt"
 	"net/http"
@@ -10,6 +13,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// 用以指定处理 HTTP 请求的函数
 	http.HandleFunc("/", handlerFunc)
+	// 用以监听本地 3000 端口并提供服务
 	http.ListenAndServe(":3000", nil)
 }
