2720b95659f5aea1a691653842840dc395fcceac
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-10-04 11:26:56 +0800

    5.4 - 插入数据

--- learn_ku/goblog/main.go
+++ learn_ku/goblog/main.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"log"
+	"strconv"
 	"time"
 
 	// 匿名导入
@@ -114,7 +115,6 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
 	title := r.PostFormValue("title")
 	body := r.PostFormValue("body")
-
 	errors := make(map[string]string)
 
 	// 验证标题
@@ -133,25 +133,26 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 
 	// 检查是否有错误
 	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过!<br>")
-		fmt.Fprintf(w, "title 的值为: %v <br>", title)
-		fmt.Fprintf(w, "title 的长度为: %v <br>", utf8.RuneCountInString(title))
-		fmt.Fprintf(w, "body 的值为: %v <br>", body)
-		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
+		lastInsertId, err := saveArticleToDB(title, body)
+		if lastInsertId > 0 {
+			fmt.Fprint(w, "插入成功， ID 为"+strconv.FormatInt(lastInsertId, 10))
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
 	} else {
 		storeURL, _ := router.Get("articles.store").URL()
-
 		data := ArticlesFormData{
 			Title:  title,
 			Body:   body,
 			URL:    storeURL,
 			Errors: errors,
 		}
-		tmpl, err := template.New("create-form").ParseFiles("resources/views/articles/create.gohtml")
+		tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
 		if err != nil {
 			panic(err)
 		}
-
 		tmpl.Execute(w, data)
 	}
 }
@@ -166,24 +167,54 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+func saveArticleToDB(title string, body string) (int64, error) {
+	// 变量初始化
+	var (
+		id   int64
+		err  error
+		rs   sql.Result
+		stmt *sql.Stmt
+	)
+
+	// 1. 获取一个 prepare 声明语句
+	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
+	// 例行的错误检测
+	if err != nil {
+		return 0, err
+	}
+
+	// 2. 在此函数运行结束后关闭此语句，防止占用 SQL 连接
+	defer stmt.Close()
+
+	// 3. 执行请求，传参进入绑定的内容
+	rs, err = stmt.Exec(title, body)
+	if err != nil {
+		return 0, err
+	}
+
+	// 4. 插入成功的话，会返回自增 ID
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+
+	return 0, err
+}
+
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
-	html := `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>创建文章 —— 我的技术博客</title>
-</head>
-<body>
-    <form action="%s?test=data" method="post">
-        <p><input type="text" name="title"></p>
-        <p><textarea name="body" cols="30" rows="10"></textarea></p>
-        <p><button type="submit">提交</button></p>
-    </form>
-</body>
-</html>
-`
 	storeURL, _ := router.Get("articles.store").URL()
-	fmt.Fprintf(w, html, storeURL)
+	data := ArticlesFormData{
+		Title:  "",
+		Body:   "",
+		URL:    storeURL,
+		Errors: nil,
+	}
+
+	tmpl, err := template.ParseFiles("resources/views/articles/create.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl.Execute(w, data)
 }
 
 func main() {
@@ -210,7 +241,7 @@ func main() {
 	// 通过命名路由获取 URL 示例
 	homeURL, _ := router.Get("home").URL()
 	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
+	articleURL, _ := router.Get("articles.show").URL("id", "1")
 	fmt.Println("articleURL: ", articleURL)
 
 	// 注册中间件
