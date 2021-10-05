9aab0d8dd4803732867a15f3ef814abf6ccda94a
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-10-05 21:25:01 +0800

    创建用户

--- learn_ku/goblog/app/models/model.go
+++ learn_ku/goblog/app/models/model.go
@@ -2,10 +2,15 @@ package models
 
 import (
 	"goblog/pkg/types"
+	"time"
 )
 
+// BaseModel 模型基类
 type BaseModel struct {
-	ID uint64
+	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;not null"`
+
+	CreatedAt time.Time `gorm:"column:created_at;index"`
+	UpdatedAt time.Time `gorm:"column:updated_at;index"`
 }
 
 func (b BaseModel) GetStringId() string {
--- learn_ku/goblog/bootstrap/db.go
+++ learn_ku/goblog/bootstrap/db.go
@@ -1,7 +1,10 @@
 package bootstrap
 
 import (
+	"goblog/app/models/article"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -20,4 +23,18 @@ func SetupDB() {
 	sqlDB.SetMaxIdleConns(25)
 	// 设置每个链接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+	// 创建和维护数据表结构
+	migration(db)
+}
+
+func migration(db *gorm.DB) {
+	// 自动迁移
+	// GORM 的自动迁移工具不支持版本，
+	// 只能保持字段与传参的 Struct 一致，无法删除字段。所以我们将自动迁移的代码封装到 migration() 方法里，
+	// 以后遇到需要删除字段的情况，将这些代码写到此方法即可。
+	db.AutoMigrate(
+		&user.User{},
+		&article.Article{},
+	)
 }
