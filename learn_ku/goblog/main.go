8130338bd689c324bb1ba84328beb7eea44b5db1
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-10-05 14:34:43 +0800

    重构更新文章

--- learn_ku/goblog/main.go
+++ learn_ku/goblog/main.go
@@ -401,11 +401,6 @@ func main() {
 	bootstrap.SetupDB()
 
 	router = bootstrap.SetupRoute()
-
-	// 指定不同的 HTTP 方法、路由名称
-	
-	router.HandleFunc("/articles/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
-	router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
 	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 
 	// 通过命名路由获取 URL 示例
