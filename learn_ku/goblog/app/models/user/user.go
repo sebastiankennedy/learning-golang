c0c0f2850968d9c2b34443863803520c14ec2e77
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-10-05 21:42:38 +0800

    注册表单验证

--- learn_ku/goblog/app/models/user/user.go
+++ learn_ku/goblog/app/models/user/user.go
@@ -8,7 +8,10 @@ import (
 type User struct {
 	models.BaseModel
 
-	Name     string `gorm:"column:name;type:varchar(255);not null;unique"`
-	Email    string `gorm:"column:email;type:varchar(255);default:NULL;unique;"`
-	Password string `gorm:"column:password;type:varchar(255)"`
+	// gorm：GORM 默认会将键小写化作为字段名称、去掉 column 项，默认是允许 NULL 的，估 default:NULL 也去掉
+	Name     string `gorm:"type:varchar(255);not null;unique" valid:"name"`
+	Email    string `gorm:"type:varchar(255);unique;" valid:"email"`
+	Password string `gorm:"type:varchar(255)" valid:"password"`
+	// gorm："-" —— 设置 GORM 在读写时略过此字段
+	PasswordConfirm string ` gorm:"-" valid:"password_confirm"`
 }
