621c1369de1dad50ac2b62f16aab2b26f03838c0
SebastianKennedy <sebastiankennedy@foxmail.com>
2021-10-06 12:48:36 +0800

    创建分类

--- learn_ku/goblog/routes/web.go
+++ learn_ku/goblog/routes/web.go
@@ -46,6 +46,11 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
 	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
 
+	// 文章分类
+	cc := new(controllers.CategoriesController)
+	r.HandleFunc("/categories/create", middlewares.Auth(cc.Create)).Methods("GET").Name("categories.create")
+	r.HandleFunc("/categories", middlewares.Auth(cc.Store)).Methods("POST").Name("categories.store")
+
 	// 开始会话
 	r.Use(middlewares.StartSession)
 }
